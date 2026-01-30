241c16c65ab35161c6f84ee8c0799e02c1cf7430
Titan <39844065+TitanComputer@users.noreply.github.com>
2026-01-30 19:31:26 +0330

    Refactor core port initialization logic

--- src/ui/mainwindow.cpp
+++ src/ui/mainwindow.cpp
@@ -148,15 +148,8 @@ MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWi
     runOnNewThread([=, this] {GetDeviceDetails(); });
 
     // Prepare core
-	int startPort = 19810;
-	int foundPort = -1;
-	for (int p = startPort; p < startPort + 100; p++) {
-	    if (IsPortAvailable(p)) {
-	        foundPort = p;
-	        break;
-	    }
-	}
-	Configs::dataStore->core_port = (foundPort != -1) ? foundPort : startPort;
+    Configs::dataStore->core_port = MkPort();
+    if (Configs::dataStore->core_port <= 0) Configs::dataStore->core_port = 19811;
 
     auto core_path = QApplication::applicationDirPath() + "/";
     core_path += "Core";
